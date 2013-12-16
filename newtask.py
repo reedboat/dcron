f48dc1e3ddc376cca69aef9175da6e446e5711ae
reedboat <zhqm03@gmail.com>
2013-12-16 08:28:51 +0800

    add report smaple

--- newtask.py
+++ newtask.py
@@ -15,35 +15,23 @@ from apscheduler.scripts import HttpScript, CommandScript
 if __name__ == '__main__':
 
     queue = HotQueue('job_changes')
-    #script = HttpScript(url='http://baidu.com')
-    script = CommandScript(command='ping -c 3 www.baidu.com')
+    script = HttpScript(url='http://baidu.comm')
+    store = SQLAlchemyJobStore(url='sqlite:////tmp/task.db', tablename='tasks')
+    #script = CommandScript(command='ping -c 3 www.baidu.com')
     local_tz = gettz('Asia/Chongqing')
     defaults = {'timezone': local_tz}
-    #trigger = IntervalTrigger(defaults, seconds=3)
-    trigger = DateTrigger(defaults, run_date=datetime(2013,12,11, 8, 11))
+    trigger = IntervalTrigger(defaults, seconds=60)
+    #trigger = DateTrigger(defaults, run_date=datetime(2013,12,11, 8, 11))
 
-    store = SQLAlchemyJobStore(url='sqlite:////tmp/task.db', tablename='tasks')
-    job   = store.get_job(6)
-    if not job:
-        job = Job(name='BaiduCheck2', script=script, trigger=trigger)
+    job = Job(name=u'BaiduCurlWithWrongUrl', script=script, trigger=trigger)
+
+    #print job.run()
+    now = datetime.now(local_tz)
+    next_run_time = job.compute_next_run_time(now)
+    print job.get_run_times(now+timedelta(seconds=60))
+    
+    if next_run_time:
+        print "add job"
+        print job
         store.add_job(job)
         queue.put({'opt_type':'add', 'job_id':job.id})
-    else:
-        changed = False
-        if job.trigger != trigger:
-            changed = True
-            job.trigger = trigger
-        if job.script != script:
-            changed = True
-            job.script = script
-        if changed:
-            store.update_job(job)
-            queue.put({'opt_type':'update', 'job_id':job.id})
-
-    print job
-
-    if job:
-        print job.run()
-        now = datetime.now(local_tz)
-        print job.compute_next_run_time(now)
-        print job.get_run_times(now+timedelta(seconds=60))
--- report_test.py
+++ report_test.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 # vim set expandtab
 
+import sys
+
 from dateutil.tz import gettz
 from datetime import datetime, timedelta
 from apscheduler.reporter import JobReporter
@@ -15,7 +17,22 @@ if __name__ == '__main__':
     stat = JobReporter(url='sqlite:////tmp/task.db', tablename='job_stats')
     job_id = 1
     status = 'missed'
-    run_time = datetime.now()
-    next_run_time = run_time + timedelta(seconds=3600)
-    stat.report(job_id, 'missed', run_time, next_run_time, 0)
+    base_time = datetime.now()
+    time = base_time
+    next_run_time = time + timedelta(seconds=3600)
+    stat.report(job_id, 'missed', time, next_run_time, 0)
+
+#    sys.exit()
+
+    time =  time + timedelta(seconds=60)
+    next_run_time = time + timedelta(seconds=3600)
+    stat.report(job_id, 'running', time, next_run_time, 0)
+    time =  time + timedelta(seconds=30)
+    stat.report(job_id, 'failed', time, next_run_time, 30)
+
+    time =  time + timedelta(seconds=60)
+    next_run_time = time + timedelta(seconds=3600)
+    stat.report(job_id, 'running', time, next_run_time, 0)
+    time =  time + timedelta(seconds=10)
+    stat.report(job_id, 'succed', time, next_run_time, 10)
 
